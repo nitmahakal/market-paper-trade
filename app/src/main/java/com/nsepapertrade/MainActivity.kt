19d1814971d2156514ac1021437be1fc91506d3d
nitmahakal <nitmahakal@gmail.com>
2026-08-19 22:35:39 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/nsepapertrade/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.nsepapertrade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text(
+                text = "NSE Paper Trade"
+            )
+        }
+    }
+}
