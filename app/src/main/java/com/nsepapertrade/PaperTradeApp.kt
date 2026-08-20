15878cddcf9f869586154f77f34eccce5a3df43f
nitmahakal <nitmahakal@gmail.com>
2026-08-20 15:27:30 +0530

    Update PaperTradeApp.kt

--- app/src/main/java/com/nsepapertrade/PaperTradeApp.kt
+++ app/src/main/java/com/nsepapertrade/PaperTradeApp.kt
@@ -2,7 +2,10 @@ package com.nsepapertrade
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import com.nsepapertrade.data.InstrumentRepository
+import com.nsepapertrade.data.ManualMarketDataProvider
+import com.nsepapertrade.data.MarketDataRepository
 import com.nsepapertrade.data.PaperTradeEngine
 import com.nsepapertrade.data.PaperTradeState
 import com.nsepapertrade.ui.PaperTradeScreen
@@ -10,12 +13,28 @@ import com.nsepapertrade.ui.PaperTradeScreen
 @Composable
 fun PaperTradeApp() {
 
+    val scope = rememberCoroutineScope()
+
     val engine = remember {
         PaperTradeEngine()
     }
 
+    val marketDataProvider = remember {
+        ManualMarketDataProvider()
+    }
+
+    val marketDataRepository = remember {
+        MarketDataRepository(
+            provider = marketDataProvider
+        )
+    }
+
     val state = remember {
-        PaperTradeState(engine)
+        PaperTradeState(
+            engine = engine,
+            marketDataRepository = marketDataRepository,
+            scope = scope
+        )
     }
 
     val instrumentRepository = remember {
@@ -31,6 +50,7 @@ fun PaperTradeApp() {
         message = state.message,
         instruments = instruments,
         selectedInstrument = state.selectedInstrument,
+        quote = state.quote,
 
         onInstrumentSelected = { instrument ->
             state.selectInstrument(instrument)
