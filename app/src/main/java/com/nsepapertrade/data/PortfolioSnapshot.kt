ad35ec5c920f4fbf43929ebc831739c2922e006e
nitmahakal <nitmahakal@gmail.com>
2026-09-19 02:44:45 +0530

    Update PortfolioSnapshot.kt

--- app/src/main/java/com/nsepapertrade/data/PortfolioSnapshot.kt
+++ app/src/main/java/com/nsepapertrade/data/PortfolioSnapshot.kt
@@ -4,6 +4,7 @@ data class PortfolioSnapshot(
     val availableCash: Double,
     val investedValue: Double,
     val currentValue: Double,
+    val realizedPnl: Double,
     val unrealizedPnl: Double,
     val totalValue: Double
 )
